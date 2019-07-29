f1a5fa23ac287067c2d01d9e5b29e51e9676a433
linxiajie <1106619871@qq.com>
2019-07-29 22:59:52 +0800

    init project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css'
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>React--动态效果组件</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
